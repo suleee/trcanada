ffd4c6e92f39ea7ed47305dd2ae962b0e8112e62
Jisu Lee <jisulee@DFOs-MacBook-Pro-2.local>
2017-11-27 23:21:20 -0800

    add comments section

--- themes/trvancouver/single-best.php
+++ themes/trvancouver/single-best.php
@@ -15,7 +15,7 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
 			
-
+			<?php comment_form(); ?>
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
@@ -25,14 +25,7 @@ get_header(); ?>
 
 		<?php endwhile; // End of the loop. ?>
 
-<!-- 
-		<footer class="social-icon">
-                        <div class="social-media-wrapper">
-                            <a class="social-media"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a>
-                            <a class="social-media"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a>
-                            <a class="social-media"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a>
-                        </div>
-                </footer> -->
+			
 			<?php if ( comments_open() || get_comments_number() ) :
     comments_template();
 endif;
